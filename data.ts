4b056f9ac0d5f142345037ba29c187931000f42f
Ibrahim Sule <ibraheemsulay@gmail.com>
2022-09-26 09:28:08 +0100

    added the data to the project

---
+++ data.ts
@@ -0,0 +1,48 @@
+const data = {
+  row: [
+    {
+      id: 10013432,
+      created: 1660665689,
+      usage: "Pallet/Packaging",
+      species: "Spruce",
+      drying_method: "KD",
+      grade: "Nordic Blue Book",
+      treatment: null,
+      dimensions: [
+        {
+          thickness: 40,
+          width: 50,
+          length: 1300,
+        },
+        {
+          thickness: 50,
+          width: 150,
+          length: 2000,
+        },
+      ],
+    },
+    {
+      id: 10013433,
+      created: 1660665689,
+      usage: "Pallet/Packaging",
+      species: "Spruce",
+      drying_method: "KD",
+      grade: "Nordic Blue Book",
+      treatment: null,
+      dimensions: [
+        {
+          thickness: 40,
+          width: 50,
+          length: 1300,
+        },
+        {
+          thickness: 50,
+          width: 150,
+          length: 2000,
+        },
+      ],
+    },
+  ],
+};
+
+export default data;
