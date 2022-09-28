9d7c8bd6155234bbd88c60bc5853d3a064c83432
Ibrahim Sule <ibraheemsulay@gmail.com>
2022-09-28 08:31:46 +0100

    hid the overflow of the product list when modal is enabled

--- src/components/all-products/index.tsx
+++ src/components/all-products/index.tsx
@@ -2,15 +2,18 @@ import Navbar from "./Navbar";
 import ProductHeader from "./ProductHeader";
 import SearchBar from "./SeachBar";
 import ProductList from "./ProductList";
+import { useContext } from "react";
+import { Context } from "../../assets/utils/Context";
 
 const AllProducts: React.FC = () => {
+  const { modal } = useContext(Context);
   return (
-    <>
+    <div style={{ overflow: modal ? "hidden" : "auto" }}>
       <Navbar />
       <ProductHeader />
       <SearchBar />
       <ProductList />
-    </>
+    </div>
   );
 };
 
