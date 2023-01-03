85d18e833a9bb404e9f4910ca1cf8a1f15185ece
Ibrahim Sule <ibraheemsulay@gmail.com>
2023-01-03 20:44:07 +0100

    added the head tag for SEO and web scrapping aid

--- src/pages/[ProductDetails].tsx
+++ src/pages/[ProductDetails].tsx
@@ -3,7 +3,7 @@ import dbConnect from "../lib/connectToDB";
 import { ProductModel } from "../lib/model";
 import { ProductType } from "../ts-types/dataTypes";
 import ProductDetails from "../components/page-components/ProductDetails/ProductDetails";
-
+import Head from "next/head";
 export async function getServerSideProps({ params }: Record<string, any>) {
   const id = params.ProductDetails as string;
   await dbConnect();
@@ -19,7 +19,44 @@ export async function getServerSideProps({ params }: Record<string, any>) {
 }
 
 const ProductDetailsPage: NextPage<{ data: ProductType }> = ({ data }) => (
-  <ProductDetails data={data} />
+  <>
+    <Head>
+      <title>Timberhub Product Details</title>
+      <meta name="description" content="Timberhub timber information" />
+      <meta
+        name="keywords"
+        content="wood, nature, timber, trees, timberhub, timber processing"
+      />
+      <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="twitter:image:src"
+        content="https://res.cloudinary.com/ibraheemsulay/image/upload/v1665858973/Screenshot_2022-10-15_at_7.34.19_PM_tnixkc.png"
+      />
+
+      <meta name="twitter:site" content="Timberhub product details" />
+
+      <meta name="twitter:title" content="Timberhub product details" />
+      <meta
+        name="twitter:description"
+        content="Timberhub available wood product information"
+      />
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/ibraheemsulay/image/upload/v1665858973/Screenshot_2022-10-15_at_7.34.19_PM_tnixkc.png"
+      />
+      <meta property="og:site_name" content="Ibrahim Sule" />
+
+      <meta property="og:title" content="Ibrahim Sule" />
+
+      <meta property="og:url" content="https://timberhub-task.vercel.app" />
+
+      <meta
+        property="og:description"
+        content="Developer portfolio of Ibrahim Sule"
+      />
+    </Head>
+    <ProductDetails data={data} />
+  </>
 );
 
 export default ProductDetailsPage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import ContextWrapper from "../components/others/ContextWrapper/ContextWrapper";
 import Products from "../components/page-components/Products/Products";
 import AddProductModal from "../components/page-components/Products/AddProductModal/AddProductModal";
@@ -23,6 +24,42 @@ export async function getServerSideProps() {
 const Home: NextPage<{ data: ProductType[] }> = ({ data }) => (
   <ContextWrapper fetchedData={data}>
     <>
+      <Head>
+        <title>Timberhub Products Page</title>
+        <meta name="description" content="Timberhub available wood lists" />
+        <meta
+          name="keywords"
+          content="wood, nature, timber, trees, timberhub, timber processing"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="twitter:image:src"
+          content="https://res.cloudinary.com/ibraheemsulay/image/upload/v1665858973/Screenshot_2022-10-15_at_7.34.19_PM_tnixkc.png"
+        />
+
+        <meta name="twitter:site" content="Timberhub products" />
+
+        <meta name="twitter:title" content="Timberhub products" />
+        <meta
+          name="twitter:description"
+          content="Timberhub available wood products"
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/ibraheemsulay/image/upload/v1665858973/Screenshot_2022-10-15_at_7.34.19_PM_tnixkc.png"
+        />
+        <meta property="og:site_name" content="Ibrahim Sule" />
+
+        <meta property="og:title" content="Ibrahim Sule" />
+
+        <meta property="og:url" content="https://timberhub-task.vercel.app" />
+
+        <meta
+          property="og:description"
+          content="Developer portfolio of Ibrahim Sule"
+        />
+      </Head>
+
       <Products />
       <AddProductModal />
     </>
